f0ecf1103327eb310f1ac64c8981a8894d132a4c
MissBreton <breton.andria@gmail.com>
2024-12-03 15:39:09 -0700

    Custom Javascript Part 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,19 @@
-console.log('javascript connected!');
-
 const carousel = new bootstrap.Carousel('#homeCarousel', {
-    interval: 2000,
+    interval: 5000,
     pause: false
-});
+})
 
-const carouselPause = document.getElementById('carouselPause');
-carouselPause.addEventListener('click', function () {
-    console.log('pausing the carousel');
-    carousel.pause();
-});
+const carouselButton = document.getElementById('carouselButton');
+const faIcon = document.getElementById('faButton');
 
-const carouselPlay = document.getElementById('carouselPlay');
-carouselPlay.addEventListener('click', function () {
-    console.log('cycle the carousel');
-    carousel.cycle();
+carouselButton.addEventListener('click', function () {
+    if (faIcon.classList.contains('fa-pause')) {
+        faIcon.classList.remove('fa-pause');
+        faIcon.classList.add('fa-play');
+        carousel.pause();
+    } else {
+        faIcon.classList.remove('fa-play');
+        faIcon.classList.add('fa-pause');
+        carousel.cycle();
+    }
 });
